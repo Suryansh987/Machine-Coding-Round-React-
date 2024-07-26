8826515d81b84343042f58d57502478927add9c3
Suryansh <suryanshsharma987@gmail.com>
2024-07-26 10:17:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ function App() {
 
   return (
     <>
+      <nav className='flex justify-center gap-10 text-xl bg-white py-2'>
+      <Link to='/' className='font-bold cursor-pointer'>Otp</Link>
+      <Link to='/course-list' className='font-bold cursor-pointer'>Course List</Link>
+      <Link to='/table' className='font-bold cursor-pointer'>Table</Link>
+    </nav>
       <h1 className={`text-6xl text-center font-inter font-[700] pt-20 
         ${pathname === '/course-list'? 'text-[#4F6F52]' : (pathname === '/table' ? 'text-[#444B79]' : 'text-white')} mb-8`}>Chai Aur Code</h1>
       <div className="fixed bottom-10 right-10 rounded-2xl overflow-hidden">
